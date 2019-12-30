9226a8b5cf92a57a3e655b971b44dd15aa6fd05a
Yangzixuan <yzx137@gmail.com>
2019-12-30 10:12:16 +0800

    build

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? './' : '/public',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/my-cnode/dist/'
+    : '/'
 }
